eee606ff9b279a97a2c45fcb9197641f5e62caee
dyankastutara <dyankastutara19@gmail.com>
2017-07-29 17:48:31 +0700

    add helpers encrypt-decrypt text

--- test/user-test.js
+++ test/user-test.js
@@ -14,24 +14,35 @@ describe('User Test', ()=>{
 		})
 	})
 })
-// var crypto = require('crypto'),
-//     algorithm = 'aes-256-ctr',
-//     password = 'd6F3Efeq';
 
-// function encrypt(text){
-//   var cipher = crypto.createCipher(algorithm,password)
-//   var crypted = cipher.update(text,'utf8','hex')
-//   crypted += cipher.final('hex');
-//   return crypted;
+
+// 'use strict';
+
+// const crypto = require('crypto');
+
+// const ENCRYPTION_KEY = process.env.ENCRYPTION_KEY; // Must be 256 bytes (32 characters)
+// const IV_LENGTH = 16; // For AES, this is always 16
+
+// function encrypt(text) {
+//  let iv = crypto.randomBytes(IV_LENGTH);
+//  let cipher = crypto.createCipheriv('aes-256-cbc', new Buffer(ENCRYPTION_KEY), iv);
+//  let encrypted = cipher.update(text);
+
+//  encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+//  return iv.toString('hex') + ':' + encrypted.toString('hex');
 // }
- 
-// function decrypt(text){
-//   var decipher = crypto.createDecipher(algorithm,password)
-//   var dec = decipher.update(text,'hex','utf8')
-//   dec += decipher.final('utf8');
-//   return dec;
+
+// function decrypt(text) {
+//  let textParts = text.split(':');
+//  let iv = new Buffer(textParts.shift(), 'hex');
+//  let encryptedText = new Buffer(textParts.join(':'), 'hex');
+//  let decipher = crypto.createDecipheriv('aes-256-cbc', new Buffer(ENCRYPTION_KEY), iv);
+//  let decrypted = decipher.update(encryptedText);
+
+//  decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+//  return decrypted.toString();
 // }
- 
-// var hw = encrypt("hello world")
-// // outputs hello world
-// console.log(decrypt(hw));
+
+// module.exports = { decrypt, encrypt };
